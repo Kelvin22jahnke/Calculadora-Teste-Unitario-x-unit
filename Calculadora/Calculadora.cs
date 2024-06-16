d2ec99f69be80ac1d4f6ad37a957e438c0498d0c
kelvin22jahnke <kelvin18jahnke@gmail.com>
2024-06-16 17:48:51 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculadora
+{
+    public static class Calculadora
+    {
+        public static int Somar(int valor1, int valor2) 
+        {
+            return valor1 + valor2;
+        }
+
+        public static int Subtrair(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public static int Multiplicar(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+
+        public static int Dividir(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
